5625b774368d1eaaa6d7f71df12b76dae30b1108
Rohan Khalate <rohan@Rohans-MacBook-Air.local>
2025-02-17 23:34:54 +0530

    Added new Budegt Form

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Main, { mainLoader } from "./layouts/Main";
+
+import { ToastContainer } from "react-toastify";
+
+import { logoutAction } from "./actions/logout";
+
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
+import Error from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        action: dashboardAction,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
