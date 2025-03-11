433ccc2d9dadf7e8bc9dff94f858eb7372f5d801
Rohan Khalate <rohan@Rohans-MacBook-Air.local>
2025-03-12 00:01:41 +0530

    added link for expenses more than 9

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { logoutAction } from "./actions/logout";
 
 import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
+import { expensesLoader, ExpensesPage } from "./pages/ExpensesPage";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,11 @@ const router = createBrowserRouter([
         loader: dashboardLoader,
         action: dashboardAction,
         errorElement: <Error />,
+      },
+      {
+        path: "expenses",
+        element: <ExpensesPage />,
+        loader: expensesLoader,
       },
       {
         path: "logout",
